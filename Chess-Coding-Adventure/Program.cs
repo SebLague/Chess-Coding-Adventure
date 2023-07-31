138eeb7e5d5d799013cd19f1cc53736adebf62cc
Sebastian Lague <sebastlague@gmail.com>
2023-07-31 11:05:42 +0200

    Create console app

---
+++ Chess-Coding-Adventure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chess_Coding_Adventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
