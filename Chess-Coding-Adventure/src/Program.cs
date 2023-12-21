f346d2d2ee92422614de063724796537337d702b
Sebastian Lague <sebastlague@gmail.com>
2023-12-21 10:41:03 +0100

    Cleanup

--- Chess-Coding-Adventure/src/Program.cs
+++ Chess-Coding-Adventure/src/Program.cs
@@ -9,9 +9,6 @@ public static class Program
     {
         EngineUCI engine = new();
 
-        GenOpenings();
-
-
         string command = String.Empty;
         while (command != "quit")
         {
@@ -20,61 +17,5 @@ public static class Program
         }
 
     }
-
-    static void GenOpenings()
-    {
-        var book = new OpeningBook(Chess_Coding_Adventure.Properties.Resources.Book);
-        HashSet<string> used = new();
-        List<Opening> openings = new();
-        Board board = new Board();
-
-        const int numPly = 2 * 8;
-
-        while(openings.Count < 512)
-        {
-            TryAddOpening();
-        }
-        Console.WriteLine(openings.Count);
-        string data = System.Text.Json.JsonSerializer.Serialize(openings.ToArray());
-        Console.WriteLine(data);
-
-        void TryAddOpening()
-        {
-            board.LoadStartPosition();
-            List<string> moveList = new();
-
-            for (int i = 0; i < numPly; i++)
-            {
-                if (book.TryGetBookMove(board, out string moveString))
-                {
-                    var move = MoveUtility.GetMoveFromUCIName(moveString, board);
-                    moveList.Add(moveString);
-                    board.MakeMove(move);
-                }
-                else
-                {
-                    return;
-                }
-
-            }
-
-            string fen = FenUtility.CurrentFen(board).Split(" ")[0];
-            if (!used.Contains(fen))
-            {
-                used.Add(fen);
-                openings.Add(new Opening() { moves = moveList.ToArray() });
-            }
-
-
-        }
-
-    }
-
-    public struct Opening
-    {
-        public string[] moves { get; set; }
-    }
-
-
 
 }
